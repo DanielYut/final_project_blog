7d9fb363a55e9b27736f2e7aba43d9c3dcd7fd05
DanielYut <daniel950401@gmail.com>
2025-12-11 06:45:11 +0800

    searchfix

---
+++ models/comment.py
@@ -0,0 +1,12 @@
+from extensions import db
+from datetime import datetime
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    author = db.Column(db.String(50), default="訪客")
+    content = db.Column(db.Text, nullable=False)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
